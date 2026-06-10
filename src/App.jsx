9771dbb7426c7642d7205e51ee88832297c39692
Juanan <juanantonioamillopez@gmail.com>
2026-06-10 11:45:11 +0200

    FEAT: Enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+import { Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
-
   return (
-    <>
-      <section id='center'>
-        <h1>Arquitecturas en React</h1>
-        <p>
-          Aquí tenemos una serie de arquitecturas para organizar nuestros proyectos en React, cada una con sus propias
-          ventajas y desventajas. La elección de la arquitectura adecuada depende de las necesidades específicas de tu
-          proyecto, el tamaño del equipo y tus preferencias personales. A continuación, se presentan algunas de las
-          arquitecturas más comunes en React:
-        </p>
-      </section>
-    </>
+    <section id='center'>
+      <h1>Arquitecturas en React</h1>
+      <p>
+        Aquí tenemos una serie de arquitecturas para organizar nuestros proyectos en React, cada una con sus propias
+        ventajas y desventajas. La elección de la arquitectura adecuada depende de las necesidades específicas de tu
+        proyecto, el tamaño del equipo y tus preferencias personales. A continuación, se presentan algunas de las
+        arquitecturas más comunes en React:
+      </p>
+
+      <nav>
+        <Link to='/mvc'>MVC</Link> |<Link to='/clean'>Clean</Link> |<Link to='/fsd'>FSD</Link> |
+        <Link to='/modular'>Modular</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/mvc' element={<>MVCPage</>} />
+        <Route path='/clean' element={<>CleanPage</>} />
+        <Route path='/fsd' element={<>FSDPage</>} />
+        <Route path='/modular' element={<>ModularPage</>} />
+      </Routes>
+    </section>
   );
 }
 
