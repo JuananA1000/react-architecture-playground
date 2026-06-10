d32fff6b992ac932b293761209bbfd5ed99689dd
Juanan <juanantonioamillopez@gmail.com>
2026-06-10 10:40:27 +0200

    PROJECT: Arquitecturas en React

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <section id='center'>
+        <h1>Arquitecturas en React</h1>
+        <p>
+          Aquí tenemos una serie de arquitecturas para organizar nuestros proyectos en React, cada una con sus propias
+          ventajas y desventajas. La elección de la arquitectura adecuada depende de las necesidades específicas de tu
+          proyecto, el tamaño del equipo y tus preferencias personales. A continuación, se presentan algunas de las
+          arquitecturas más comunes en React:
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default App;
