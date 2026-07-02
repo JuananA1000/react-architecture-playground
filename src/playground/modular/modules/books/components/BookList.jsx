a5e727bb1da68acc05d12b1f0f5bfa438b2b09f5
Juanan <juanantonioamillopez@gmail.com>
2026-07-02 13:56:05 +0200

    FIX: Eliminar componentes innecesarios

--- src/playground/modular/modules/books/components/BookList.jsx
+++ src/playground/modular/modules/books/components/BookList.jsx
@@ -1,11 +1,14 @@
-import BookCard from './BookCard';
-
 const BookList = ({ books, onDelete }) => {
   return (
     <>
       <h3>Todos los libros</h3>
       {books.map((book) => (
-        <BookCard key={book.id} book={book} onDelete={onDelete} />
+        <>
+          <li>
+            {book.titulo} de {book.autor}
+            <button onClick={() => onDelete(book.id)}>Borrar Libro</button>
+          </li>
+        </>
       ))}
     </>
   );
